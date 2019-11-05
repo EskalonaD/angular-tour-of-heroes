c95782bbd63dd03ae3f87d4c5616de10e7b1f2a9
Denys Eskalona-Lopes <denys_eskalona-lopes@epam.com>
2019-11-05 17:56:44 +0200

    create messages component & service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,6 +3,7 @@ import { Observable, of } from 'rxjs';
 
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,8 @@ import { HEROES } from './mock-heroes';
 export class HeroService {
 
   getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 }
