7ff488e27b3eacbed4f17c4cb7e0c4648bb9248c
Denys Eskalona-Lopes <denys_eskalona-lopes@epam.com>
2019-11-05 14:42:07 +0200

    create heroService move forming heroes array to it

---
+++ src/app/hero.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  getHeroes(): Observable<Hero[]> {
+    return of(HEROES);
+  }
+  constructor() { }
+}
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
-import {HEROES} from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,14 +9,20 @@ import {HEROES} from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
   selectedHero: Hero;
+  heroes: Hero[];
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   };
-  heroes = HEROES;
 
-  constructor() {}
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
+
+  constructor(private heroService: HeroService) {}
 
   ngOnInit() {
+    this.getHeroes();
   }
 
 }
